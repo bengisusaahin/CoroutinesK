1a85d525705f60a89d67bb643e9c03dcf9b01dde
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-26 23:32:02 +0300

    GlobalScopevs runBlocking

--- app/src/main/java/com/bengisusahin/coroutinesk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/coroutinesk/MainActivity.kt
@@ -3,13 +3,16 @@ package com.bengisusahin.coroutinesk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
         //Light Weightness
 
         GlobalScope.launch {
@@ -19,5 +22,31 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+         */
+
+        //Scope
+        //Global Scope, runBlocking, CoroutineScope
+/*
+        //runBlocking
+        println("run blocking start")
+        runBlocking {
+            //launch coroutine başlatma
+            launch {
+                delay(5000)
+                println("run blocking")
+            }
+        }
+        println("run blocking end")
+*/
+
+        //Global Scope
+        println("Global Scope start")
+        GlobalScope.launch {
+            delay(5000)
+            println("Global Scope")
+
+        }
+        println("Global Scope end")
+
     }
 }
