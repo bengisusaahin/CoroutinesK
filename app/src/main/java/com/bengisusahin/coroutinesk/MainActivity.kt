b423f0518186413338e22d95f493b4f5a4306b82
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-26 18:09:55 +0300

    Coroutines vs thread

--- app/src/main/java/com/bengisusahin/coroutinesk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/coroutinesk/MainActivity.kt
@@ -2,10 +2,22 @@ package com.bengisusahin.coroutinesk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Light Weightness
+
+        GlobalScope.launch {
+            repeat(100000){
+                launch {
+                    println("android")
+                }
+            }
+        }
     }
 }
