ce744f3baca3ef54d5ea7506c6bc707a9f4c8288
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-28 01:49:42 +0300

    withContext and Exception Handling

--- app/src/main/java/com/bengisusahin/coroutinesk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/coroutinesk/MainActivity.kt
@@ -2,12 +2,16 @@ package com.bengisusahin.coroutinesk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,13 +58,60 @@ class MainActivity : AppCompatActivity() {
 
         //CoroutineScope
         //Context hangi verilerle çalışcağını söylüyor
-        println("coroutine scope start")
-        CoroutineScope(Dispatchers.Default ).launch {
-            delay(5000)
-            println("coroutine scope")
-        }
-        println("coroutine scope end")
+//        println("coroutine scope start")
+//        CoroutineScope(Dispatchers.Default ).launch {
+//            delay(5000)
+//            println("coroutine scope")
+//        }
+//        println("coroutine scope end")
 
+
+        //Exception Handling
+//        lifecycleScope.launch {
+//            try {
+//                throw Exception("error")
+//            }catch (e: Exception){
+//                e.printStackTrace()
+//            }
+//        }
+
+        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            println("exception: " + throwable.localizedMessage)
+        }
+
+//        lifecycleScope.launch(handler) {
+//            throw Exception("error")
+//        }
+//
+//        lifecycleScope.launch(handler) {
+//            launch {
+//                throw Exception("error2") //bir launch ın hata vermesi yeterli
+//            }
+//            launch {
+//                delay(500L)
+//                println("executed")
+//            }
+//        }
+
+
+        lifecycleScope.launch(handler) {
+            supervisorScope {
+                launch {
+                    throw Exception("error2") //supervisorle etkilemiyo dierlerini
+                }
+                launch {
+                    delay(500L)
+                    println("executed")
+                }
+
+            }
+        }
+
+        CoroutineScope(Dispatchers.Main + handler).launch {
+            launch {
+                throw Exception("error in a coroutineScope")
+            }
+        }
 
     }
 }
